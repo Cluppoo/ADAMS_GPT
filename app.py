bfad2f18130713d8cb2af798767a9d4eb5c646df
Cluppo <121154106+Cluppoo@users.noreply.github.com>
2024-09-09 16:59:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import re
 import io
 import sys
 __import__('pysqlite3')
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import pysqlite3
+sys.modules['sqlite3'] = sys.modules["pysqlite3"]
 import requests
 import wget
 
